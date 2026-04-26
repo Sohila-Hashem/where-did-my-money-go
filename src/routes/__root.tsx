Sohila-Hashem/pandacoins

feat(auth): added an auth system

src/routes/__root.tsx
@@ -2,15 +2,18 @@ import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { CustomCategoriesProvider } from '@/hooks/use-custom-categories'
 import { Toaster } from 'sonner'
 import { CustomizationToggle } from "@/components/customization-toggle";
+import { AuthProvider } from '@/hooks/use-auth'
 
 export const Route = createRootRoute({
     component: () => (
-        <CustomCategoriesProvider>
-            <Outlet />
-            <Toaster richColors position="top-center" />
-            <div className="fixed bottom-6 right-6 z-50">
-                <CustomizationToggle />
-            </div>
-        </CustomCategoriesProvider>
+        <AuthProvider>
+            <CustomCategoriesProvider>
+                <Outlet />
+                <Toaster richColors position="top-center" />
+                <div className="fixed bottom-6 right-6 z-50">
+                    <CustomizationToggle />
+                </div>
+            </CustomCategoriesProvider>
+        </AuthProvider>
     ),
 });

Sohila-Hashem/where-did-my-money-go

src/routes/__root.tsx
@@ -2,15 +2,18 @@ import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { CustomCategoriesProvider } from '@/hooks/use-custom-categories'
 import { Toaster } from 'sonner'
 import { CustomizationToggle } from "@/components/customization-toggle";
+import { AuthProvider } from '@/hooks/use-auth'
 
 export const Route = createRootRoute({
     component: () => (
-        <CustomCategoriesProvider>
-            <Outlet />
-            <Toaster richColors position="top-right" />
-            <div className="fixed bottom-6 right-6 z-50">
-                <CustomizationToggle />
-            </div>
-        </CustomCategoriesProvider>
+        <AuthProvider>
+            <CustomCategoriesProvider>
+                <Outlet />
+                <Toaster richColors position="top-right" />
+                <div className="fixed bottom-6 right-6 z-50">
+                    <CustomizationToggle />
+                </div>
+            </CustomCategoriesProvider>
+        </AuthProvider>
     ),
 });